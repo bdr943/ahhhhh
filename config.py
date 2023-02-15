38c663b4345e057c7aede0e05022859e51306c2c
bdr943 <125312594+bdr943@users.noreply.github.com>
2023-02-15 07:29:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-session = os.environ.get("TERMUX")
-SESSION = os.environ.get("TERMUX")
-token = os.environ.get("TOKEN")
+APP_ID = os.environ.get("26641701")
+APP_HASH = os.environ.get("5a0a7208eb91f3623258c4f4bb3abf0a")
+BOT_USERNAME = os.environ.get("iqthon9181766bot")
+session = os.environ.get("1ApWapzMBu4YcdmfW3nzFvDfrJ8xjCsAZd8gSrhEzsu7bj2K4dSPxXDWqrJhJuGeJxY0sQjpwRDHus-Bv1NhPoaZjYSfwuAVPkWVP3FlsZ82ocAlnT8ZqtsqlEl9N2wSY4BeZH8K6lFbDc2jCz7nyqN3NICzqDLJuRZp3jFW_yWU4OORQwWOWaF3TGfWVrI9HEOBmRo-vEoMVH2VFiIPX1oGbkixxcWeo7pbDWVSYCBIyxLgbKjiH4_0QP11LipierQ7oJlfdQ9cUffgnj8MSEAE8T8aB31Kq_1JtGVZMHIJS56qQvwW5Q3To9Vzv4xxLyAtxraHZIxBSx0qw4yXyYC6cBF_1mOw=")
+SESSION = os.environ.get("1ApWapzMBu4YcdmfW3nzFvDfrJ8xjCsAZd8gSrhEzsu7bj2K4dSPxXDWqrJhJuGeJxY0sQjpwRDHus-Bv1NhPoaZjYSfwuAVPkWVP3FlsZ82ocAlnT8ZqtsqlEl9N2wSY4BeZH8K6lFbDc2jCz7nyqN3NICzqDLJuRZp3jFW_yWU4OORQwWOWaF3TGfWVrI9HEOBmRo-vEoMVH2VFiIPX1oGbkixxcWeo7pbDWVSYCBIyxLgbKjiH4_0QP11LipierQ7oJlfdQ9cUffgnj8MSEAE8T8aB31Kq_1JtGVZMHIJS56qQvwW5Q3To9Vzv4xxLyAtxraHZIxBSx0qw4yXyYC6cBF_1mOw=")
+token = os.environ.get("5920496145:AAHa6yfqQgdQoO5DGmV4ZxGta5Ihpc7hSn0")
 fifthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 ispay = ['yes']
